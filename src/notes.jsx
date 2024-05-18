92dddbc0f1df57699613fca835d5c6bc89e25dff
Ali <89438079+AliBa1@users.noreply.github.com>
2024-05-18 16:26:43 -0700

    Update notes.jsx

--- src/notes.jsx
+++ src/notes.jsx
@@ -1,1 +1,3 @@
 // Use for card tilt: https://github.com/mkosir/react-parallax-tilt
+// Database: https://www.thesportsdb.com/free_sports_api
+// Players JSON: https://github.com/bttmly/nba/blob/master/data/players.json
